695539f2990cb36b5b7543b12083a7af7d2ffe5e
ParthSankpal <parth.sankpal47@gmail.com>
2023-12-09 10:31:53 +0530

    Added SignOut funactionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,9 @@ import {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
@@ -116,6 +119,21 @@ const Profile = () => {
   };
 
 
+  const handleSignOut =async ()=>{
+    dispatch(signOutUserStart());
+    try {
+      const res = await fetch('api/auth/signout');
+      const data = await res.json();
+      if(data.success === false) {
+        dispatch(signOutUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
+
   return (
     <div className="font-Higuen p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center ">Profile</h1>
@@ -191,7 +209,7 @@ const Profile = () => {
         >
           Delete Account
         </span>
-        <span className=" text-red-700 cursor-pointer">Sign out</span>
+        <span onClick={handleSignOut} className=" text-red-700 cursor-pointer">Sign out</span>
       </div>
 
       <p className=" text-red-700 mt-5">{error ? error : ""}</p>
