a97faaa94dc0b7ae5b9b0ac305a8131615373055
ParthSankpal <parth.sankpal47@gmail.com>
2023-12-31 16:51:40 +0530

    SIgnUp issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+// import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Header from "./components/Header";
@@ -20,7 +20,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
+        {/* <Route path="/sign-up" element={<SignUp />} /> */}
         <Route path="/about" element={<About />} />
         <Route path='/search' element={<Search />} />
         <Route path="/listing/:listingId" element={<Listing/>}/>
