4d9577fc71ec6a57a91d3dcc82fa58bf51d29a7f
ParthSankpal <parth.sankpal47@gmail.com>
2024-01-25 19:43:21 +0530

    Resolved signpu issue for deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-// import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
-import Search from "./pages/Search";
 import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
 
@@ -23,7 +21,6 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />
         
         <Route path="/about" element={<About />} />
-        <Route path='/search' element={<Search />} />
         <Route path="/post/:postId" element={<Post/>}/>
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
